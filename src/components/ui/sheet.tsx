d8d8ef56978f60eeaa0a2a9a3b0458ba03657832
sami <samilucerom@gmail.com>
2026-02-09 04:03:09 +0000

    tiene que estar mas cerca de la imagen y un poco mas notorio  (_for elem

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -65,9 +65,9 @@ const SheetContent = React.forwardRef<
       {...props}
     >
       {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
+      <SheetPrimitive.Close className="absolute right-2 top-2 rounded-full p-2 bg-background/80 backdrop-blur-md opacity-90 ring-offset-background transition-all hover:opacity-100 hover:scale-110 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary shadow-md z-[60]">
+        <X className="h-6 w-6" />
+        <span className="sr-only">Cerrar</span>
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
   </SheetPortal>
@@ -97,7 +97,7 @@ const SheetFooter = ({
       "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
       className
     )}
-    {...props}
+    ...props
   />
 )
 SheetFooter.displayName = "SheetFooter"
